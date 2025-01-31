82a808a34ecfba6e10a4d36d77deb0d177a53c9f
maishaparedes <maisha.paredes@ucas-edu.net>
2025-01-31 11:28:44 -0700

    firstc

---
+++ variables/practice.c
@@ -0,0 +1,22 @@
+char name[] = "Maisha";
+
+int num = 5;
+
+int num_2 = 128;
+
+char breakfast[] = "smoothie";
+
+char color[] = "pink";
+
+char school[] = "UCAS";
+
+int year = 2025;
+
+char eye_color[] = "brown";
+
+int age = 14;
+
+char favorite_subject[] = "english";
+
+
+
