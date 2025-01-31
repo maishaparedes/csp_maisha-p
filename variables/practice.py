a3674609ea3bcbff4d263958468f03f755e55a88
maishaparedes <maisha.paredes@ucas-edu.net>
2025-01-31 11:37:13 -0700

    python practice

--- variables/practice.py
+++ variables/practice.py
@@ -2,3 +2,23 @@
 
 name = "maisha"
 
+num = 5
+
+num_2 = 989
+
+breakfast = "smoothie"
+
+favorite_color = "pink"
+
+school = "UCAS"
+
+year = 2025
+
+eye_color = "brown"
+
+age = 14
+
+favorite_subject = "english"
+
+
+
