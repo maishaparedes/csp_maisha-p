e8955fb63672b873fedba871d828aec8d517630f
maishaparedes <maisha.paredes@ucas-edu.net>
2025-02-05 11:39:14 -0700

    expressions

--- expressions/expressions.c
+++ expressions/expressions.c
@@ -3,11 +3,11 @@
 
 
 int main(void){
-    float equation = (7-24/8*4+6);
-    int num = 1;
+
 printf(7-24/8*4+6);
 printf(18/3-7+2*5);
- 
+printf(6*4/12+72/8-9);
+printf([17-6/2]+4*3);
     return 0;
     }
 
--- expressions/notes.c
+++ expressions/notes.c
@@ -5,13 +5,15 @@
 //float equation =5*7+11/(5-x);
 int num = 12;
 float pi = 3.1415926526535;
-
+float expression = 5*7+4
 // + - * / //
 int main(void){
-    num = 4
-    prinf("%d\n", num)
+    //num = 4
+    prinf("%d\n", num);
     //printf(%f\n", equation);
-
-    printf("%.10f", pi)
+    //printf("%d\n", num);
+    printf("%.f", expression);
+    printf((int)6+9/2);
+    //printf("%.20f", pi)
     return 0;
     }
