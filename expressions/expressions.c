f2b1774f88d9f6a9e44383d20c3bc0beb7a73422
maishaparedes <maisha.paredes@ucas-edu.net>
2025-02-05 12:35:13 -0700

    expressions.c

--- expressions/expressions.c
+++ expressions/expressions.c
@@ -1,38 +1,30 @@
 //Expressions practice, Maisha Paredes
-#include <stdio.h>
+
+#include <stdio.h>  
 #include <math.h>
 
-int var_one = (7-24/8*4+6)
-
-int var_two = (18/3-7+2*5)
-
-int var_three = (6*4/12+72/8-9)
-
-int var_four = ((17-6/2)+4*3)
-
-int var_five = (-2*(1*4-2/2)+(6+2-3))
-
-int var_six = (-1*((3-4*7)/5)-2*24/6)
-
-int var_seven  = ((3*5**2/15)-(5-2**2))
-
-int var_eight = ((1**4*2**2+3**3)-2**5/4)
-
-int var_nine = ((22/2-2*5)**2 +(4-6/6)**2)
+int var_one = 7-24/8*4+6;
 
 
-int main(void){
-printf("%d, %d, %d, " print
-    return 0;
-    }
+int var_two = 18/3-7+2*5;
 
 
+int var_three = 6*4/12+72/8-9;
 
 
-//print(70+8) #addition
-//print(70-8) #subtraction
-//print(70*8) #multiplication
-//print(70/8) #division
-//print(70**8) #exponents
-//print(70//8) #Integer division, returns only the whole number
-//print(70%8)#Mdulo/modulus/MOD returns the remainder 
+int var_four = (17 - 6 / 2) + 4 * 3;
+
+
+int var_five = -2*(1*4-2/2)+(6+2-3);
+
+
+int var_six = -1 * ((3 - 4 * 7) / 5) - 2 * 24 / 6;
+
+
+int var_seven = (3*pow(5,2) / 15) - (5 - pow(2,2));
+
+int main(void) {  
+    
+printf("%d,%d,%d,%d,%d,%d,%d", var_one, var_two, var_three, var_four, var_five, var_six, var_seven);  
+return 0;  
+} 
