f1e8904979c07347e08d5726158ee77b6ec89ac1
maishaparedes <maisha.paredes@ucas-edu.net>
2025-02-06 11:49:23 -0700

    finantialcalc

---
+++ expressions/finantial_calc.py
@@ -0,0 +1,60 @@
+#Maisha Paredes, Finantial Calculator python
+
+# print statement that welcomes user and tells us what program does
+
+print("Welcome! This program will calculate your savings and how much you have left to spend. You can only type the number, don't put dollars or dollar signs!")
+
+#ask user what their monthly income is (variable, an input)
+
+income = float(input("What is your income?\n"))
+
+#ask user what thier rent is (variable, an input)
+
+rent = float(input("How much is your rent?\n"))
+#ask user what their utilities is (variable, an input)
+
+utilities = float(input("How much are your utilities?\n"))
+
+#ask user what their groceries is (variable, an input)
+
+groceries = float(input("How much are your groceries?\n"))
+
+#ask user what their transportation is (variable, an input)
+
+transport = float(input("How much is your transportation?\n"))
+
+#calculate savings as 10% of income (income*.1) (variable)
+
+savings = income * 0.1
+
+#calculate spending as income-savings-rent-utilities-groceries-transport (variable)
+
+spendings = 
+#calculate percent income of rent (rent/income *100)  (variable)
+
+
+#calculate percent income of utilities (utilities/income *100)  (variable)
+
+
+#calculate percent income of groceries (groceries/income *100)  (variable)
+
+#calculate percent income of transportation (transportation/income *100)  (variable)
+
+#calculate percent income of spending (spending/income *100)  (variable)
+
+#your rent is $XX.XX which is XX% of your income. (Print)
+
+#your utilities is $XX.XX which is XX% of your income. (Print)
+
+#your groceries is $XX.XX which is XX% of your income. (Print)
+
+#your transportation is $XX.XX which is XX% of your income. (Print)
+
+#your savings are $XX.XX which is XX% of your income. (Print)
+
+#your spendings are $XX.XX which is XX% of your income. (Print)
+
+
+
+
+
---
+++ expressions/finantialcalc.c
@@ -0,0 +1,3 @@
+//Maisha Paredes, Finantial Calculator
+
+#include <stdio.h>
