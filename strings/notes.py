18db3d801e066bb932f70d6c7b00b66da383dbc8
maishaparedes <maisha.paredes@ucas-edu.net>
2025-02-10 11:49:10 -0700

    stringd

--- strings/notes.py
+++ strings/notes.py
@@ -1,3 +1,13 @@
 #Maisha Paredes, String notes python
 
 #String is a data type that holds information surrounded by quotation marks like: "" ''
+
+note = "Teachers class"
+
+name = input("What is your first name?").strip().lower()
+
+#print(f"Hello {name} welcome to my program!")
+
+print(f"this is your name {name}")
+
+sentence = "The quick brown fox jumps over the lazy dog"
