0853440301ee3ae049551b76fb300d9577dca8aa
maishaparedes <maisha.paredes@ucas-edu.net>
2025-02-11 11:49:00 -0700

    commitnotesc

---
+++ strings/notes.c
@@ -0,0 +1,35 @@
+// Maisha Paredes, notes c
+#include <stdio.h>
+#include <string.h>
+
+char subject[50];
+char name[] = "Victoria";\
+char sentence[] = "The quick brown fox jumps over the lazy dog."
+
+int main(void){
+   //printf("What class are you in?");
+   //scanf("%s", subject);
+   //fgets(subject, sizeof(subject), stdin);
+   //printf("Dude, your in %s, ? that is a COOL class!", subject);
+
+   //printf("%s", name);
+   //name[0] = "T";;
+   //name[1] = "o";
+   //name[2] = "r";
+   //name[3] = "i";
+   //printf("%lu\n", sizeof(sentence));
+   //printf(strlen(sentence));
+
+char one[] = "Hello";
+
+char two[] = "World";
+
+char three[] = "Welcome to my program.";
+
+printf("%s\n", one);
+strcat(one, two);
+printf("%s\n", one);
+strcat(three, one);
+
+    return 0;
+    }
