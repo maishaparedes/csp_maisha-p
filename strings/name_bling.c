b92ffd28e3ed6477fe74a299d65dd043b212d883
maishaparedes <maisha.paredes@ucas-edu.net>
2025-02-12 11:33:26 -0700

    namedec

---
+++ strings/name_bling.c
@@ -0,0 +1,15 @@
+// Maisha Paredes, name decorator C
+#include <stdio.h>
+#include <math.h>
+
+int main() {  
+    char name[50]; 
+  
+    printf("What is your name dude: ");   
+    scanf("%s", name); 
+  
+ 
+    printf(">:P %s >_<\n", name);  
+  
+    return 0; +
+}  
---
+++ strings/name_bling.py
@@ -0,0 +1,6 @@
+#Maisha paredes, Name decorator python 
+
+
+name = input("What is your name dude: ")  
+
+print(f">:) {name} >:(")  
