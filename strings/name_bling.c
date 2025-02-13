672f550b9c291da78f11c186893ea6dd86917b97
maishaparedes <maisha.paredes@ucas-edu.net>
2025-02-13 07:16:27 -0700

    name_bling.c

--- strings/name_bling.c
+++ strings/name_bling.c
@@ -1,15 +1,19 @@
 // Maisha Paredes, name decorator C
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
-int main() {  
-    char name[50]; 
-  
-    printf("What is your name dude: ");   
-    scanf("%s", name); 
-  
- 
-    printf(">:P %s >_<\n", name);  
-  
+int main() {
+    char name[50];
+    char decoration[60] = ">:)"; 
+
+    printf("What is your name dude: ");
+    scanf("%s", name);
+
+    strcat(output, " "); 
+    strcat(output, name); 
+    strcat(output, " >:("); 
+    printf("%s\n", output);
+
     return 0;
 }  
