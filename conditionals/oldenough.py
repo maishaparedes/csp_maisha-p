88b5eb95c04c6641ede83b095ef8b82f9a20ce7d
maishaparedes <maisha.paredes@ucas-edu.net>
2025-02-26 11:19:00 -0700

    old_enuf

---
+++ conditionals/oldenough.py
@@ -0,0 +1,17 @@
+#Maisha tapia, old enough python
+
+age = int(input("Let's see if you can vote and/or drive, \n as well as go to school! but first..How old are you? "))  
+  
+if age >= 18:  
+    print("You can vote!")  
+elif age >= 17:  
+    print("You can drive!")  
+elif age >= 15:  
+    print("You can get a learner's permit!")  
+elif age >= 6:   
+    print("Wow. You can go to school and learn! Nice")  
+else:  
+    print("Bad news.\n You aren't old enough to vote, drive, or get a learner's permit. \n Run this code when your older i guess, good luck with kindergarden.")  
+
+
+
