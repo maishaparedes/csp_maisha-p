0fa7d1550f7e0615ec400a6e040a91d394aefe6d
maishaparedes <maisha.paredes@ucas-edu.net>
2025-03-06 11:18:11 -0700

    familyloop

---
+++ loops/loopfam.c
@@ -0,0 +1,14 @@
+// maisha paredes, My family loop C
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+const char*  siblings[] = {"Angie", "Hanah", "Josh", "Ethan", "Oliver", "Maisha"};   
+
+for (int i = 0; i < 6; i++) {  
+    printf("Hello, %s!\n", siblings[i]);  
+}  
+return 0;  
+
+}
---
+++ loops/myfamily.py
@@ -0,0 +1,6 @@
+#Maisha Paredes, my family loop python
+
+siblings = ["Angie", "Hanah", "Josh", "Ethan", "Oliver", "Maisha"]  
+  
+for name in siblings:  
+    print(f"Hello, {name}!")  
