ce096d58b6650134f19f179dffe9617b698022c7
maishaparedes <maisha.paredes@ucas-edu.net>
2025-03-07 11:11:13 -0700

    firstprogram

---
+++ variables/firstprogram.c
@@ -0,0 +1,11 @@
+// Maisha Paredes, first program c
+#include <stdio.h>  
+  
+int main(void) {  
+    char name[50];   
+    printf("What's your name? Enter: "); 
+    scanf("%s", name);   
+    printf("Hiii, %s!", name);   
+  
+    return 0;  
+}  
