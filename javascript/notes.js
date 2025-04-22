b1ed791e067206f67505fc7aa404a9ed699eb188
maisha.paredes@ucas-edu.net <maisha.paredes@ucas-edu.net>
2025-04-22 11:49:07 -0600

    bbpush

---
+++ javascript/notes.js
@@ -0,0 +1,9 @@
+let images = ["]
+    
+    function hello(){
+    document.getElementById("title").innerHTML = "Hello world!"
+}
+function hover(){
+    document.getElementById("img").src = "https://cdn10.bigcommerce.com/s-npe4l/products/1158/images/1331/b3-mz-mad---high__78212.1716327968.1280.1280.jpg?c=2"
+}
+}
