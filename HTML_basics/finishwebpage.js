0c916dbdfcc7ff2203c51239154afbdf5c14808e
maisha.paredes@ucas-edu.net <maisha.paredes@ucas-edu.net>
2025-04-30 13:31:11 -0600

    fast

--- HTML_basics/finishwebpage.js
+++ HTML_basics/finishwebpage.js
@@ -2,3 +2,13 @@ function show(){
    document.getElementById("placing").style.display = "block"
       
 }
+
+
+function view(){
+   if(document.getElementById("other").style.display ==="block"){
+           document.getElementById("other").style.display = "none"
+   }
+   else{
+       document.getElementById("other").style.display = "block"
+   }
+}
