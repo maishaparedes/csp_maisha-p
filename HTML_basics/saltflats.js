0bd89cf105eea53b543501d1c4d890ee245ce5a1
maisha.paredes@ucas-edu.net <maisha.paredes@ucas-edu.net>
2025-05-05 11:49:04 -0600

    push

---
+++ HTML_basics/saltflats.js
@@ -0,0 +1,15 @@
+function show(){
+    document.getElementById("placing").style.display = "block"
+       
+ }
+ 
+ 
+ function view(){
+    if(document.getElementById("secret").style.display ==="block"){
+            document.getElementById("secret").style.display = "none"
+    }
+    else{
+        document.getElementById("secret").style.display = "block"
+    }
+ }
+ 
